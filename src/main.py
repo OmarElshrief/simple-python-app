1ba248b84545c3204e9d8322687e2fa3457b326b
Omar ElShrief <omar.elshrief@ibm.com>
2025-09-17 10:49:00 +0300

    changing to flask app

--- src/main.py
+++ src/main.py
@@ -1,24 +1,12 @@
-"""
-This program counts the number of words in a string. Also display the result for each word and the total.
-"""
-import string as str
+from flask import Flask
+
+app = Flask(__name__)
 
 
-def count_words(str_func):
-    my_dic = {}
-    # creating the dictionary:
-    for letter in str_func.split():
-        my_dic[letter] = 0
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
-    # Counting the words:
-    for word in str_func.split():
-        my_dic[word] += 1
-    return my_dic
-
-
+# main driver function
 if __name__ == '__main__':
-    str_input = input("Enter a word, phrase or sentence: ")
-    words_dic = count_words(str_input)
-    print("The number of words:")
-    print(words_dic)
-    print("The total number of words is: {}".format(sum(words_dic.values())))
+    app.run()
