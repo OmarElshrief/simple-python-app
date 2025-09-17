43253da099e91aca50cabd1bc139471a5e089a4e
Omar ElShrief <omar.elshrief@ibm.com>
2025-09-17 10:52:45 +0300

    adding endpoits

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/v1/details')
 def hello_world():
     return 'Hello World'
 
 # main driver function
 if __name__ == '__main__':
     app.run()
+
+
+# /api/v1/healthz
